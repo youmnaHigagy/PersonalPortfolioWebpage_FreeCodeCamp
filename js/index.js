68c981a8b7d103f6a95b63e9e97b435cf394e8c9
youmnaHigagy <youmnahigagy@gmail.com>
2016-07-06 15:34:10 +0200

    contact and footer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+$(function() {
+  $('body').attr("id", "page-top")
+    .attr("data-spy", "scroll")
+    .attr("data-target", ".navbar-fixed-top");
+});
+
  $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -5,7 +11,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top - 70
+          scrollTop: target.offset().top
         }, 1000);
         return false;
       }
